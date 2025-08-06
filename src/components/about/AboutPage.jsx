cf4945e144e8855a488d3988914de3b477332e6e
Diwakar <singhdiwakar3208@gmail.com>
2025-08-06 16:23:52 +0530

    done

--- src/components/about/AboutPage.jsx
+++ src/components/about/AboutPage.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import Hero from './Hero'
+
 import Story from './Story'
 
 const AboutPage = () => {
   return (
     <div>
-      <Hero />
       <Story/>
     </div>
   )
